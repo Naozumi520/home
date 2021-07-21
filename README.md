a5ef5dcd98938d6f2cb4ff86d63b8f111353acdc
Naozumi520 <Naozumi0512@gmail.com>
2021-07-21 18:59:33 +0800

    roll back

--- README.md
+++
@@ -1,5 +0,0 @@
-### Credits: Archiruz
-https://vincentgarreau.com/particles.js for Particles.js
-
-https://freehtml5.co for Parts of CSS
-
--- js/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('click', musicPlay);
-function musicPlay() {
-    var audio = document.getElementById('audioID');
-    audio.play();
-    audio.volume = 0.5;
-    document.removeEventListener('click', musicPlay);
-}
